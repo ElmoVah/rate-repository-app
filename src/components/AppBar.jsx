dfefb65a1aed75cb196f3c13a2b6c8992e3844ac
Elmo Vahvaselkä <elmo.vahvaselka@hotmail.com>
2022-09-04 08:43:39 +0300

    Exercise 10.7: scrollable app bar
    As we are adding more tabs to our app bar, it is a good idea to allow horizontal scrolling once the tabs won't fit the screen. The ScrollView component is just the right component for the job.
    
    Wrap the tabs in the AppBar component's tabs with a ScrollView component:
    
    Setting the horizontal prop true will cause the ScrollView component to scroll horizontally once the content won't fit the screen. Note that, you will need to add suitable style properties to the ScrollView component so that the tabs will be laid in a row inside the flex container. You can make sure that the app bar can be scrolled horizontally by adding tabs until the last tab won't fit the screen. Just remember to remove the extra tabs once the app bar is working as intended.

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,4 +1,4 @@
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, ScrollView } from 'react-native';
 import Constants from 'expo-constants';
 import AppBarTab from './AppBarTab';
 import theme from '../theme';
@@ -8,15 +8,17 @@ const styles = StyleSheet.create({
     paddingTop: Constants.statusBarHeight,
     backgroundColor: theme.colors.tabBackground,
     display: "flex",
-    flexDirection: "row",
+    flexDirection: "row"
   }
 });
 
 const AppBar = () => {
   return (
     <View style={styles.container}>
-      <AppBarTab link="/" tabName="Repositories" style={styles.tab} />
-      <AppBarTab link="/signin" tabName="Sign in" style={styles.tab} />
+      <ScrollView horizontal>
+        <AppBarTab link="/" tabName="Repositories" style={styles.tab} />
+        <AppBarTab link="/signin" tabName="Sign in" style={styles.tab} />
+      </ScrollView>
     </View>
   );
 };
